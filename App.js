d7dfc4fcf7cd73d8a467f61300da228702d2600f
ChaitanyaRajNimap <chaitanyarajiwade@nimapinfotech.com>
2023-02-17 11:39:14 +0530

    Firestore read operation done

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 const App = () => {
+  //For storing database data
+  const [dbData, setDbData] = useState(null);
+
+  useEffect(() => {
+    //For calling function to get data from database when page render
+    getDatabase();
+  }, []);
+
+  //For getting database
+  const getDatabase = async () => {
+    try {
+      const data = await firestore()
+        .collection('testing')
+        .doc('INLltinBbNQUYl8Wp5D2')
+        .get();
+      setDbData(data._data);
+      console.log(data._data);
+    } catch (err) {
+      console.log('Error in getting database: ', err);
+    }
+  };
+
   return (
     <View style={styles.rootContainer}>
-      <Text style={styles.heading}>Firebase Integration Demo</Text>
+      <Text style={styles.heading}>
+        Name : {dbData ? dbData.name : 'Loading...'}
+      </Text>
+      <Text style={styles.heading}>
+        Age : {dbData ? dbData.age : 'Loading...'}
+      </Text>
+      <Text style={styles.heading}>
+        Hobbies : {dbData ? dbData.hobby.map(list => ` ${list}`) : 'Loading...'}
+      </Text>
     </View>
   );
 };
@@ -18,6 +49,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   heading: {
+    marginBottom: 10,
     color: '#000',
     fontSize: 25,
     fontWeight: 'bold',
